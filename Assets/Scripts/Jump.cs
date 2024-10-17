29424b54167cd53f5d22c6bd12f6240ddd18dfde
BellDylan373 <dylan.bellxd@gmail.com>
2024-10-16 22:45:46 -0500

    CrossHair/ wallslide (WIP)
    wallslide not functional
    player no longer sticks to walls

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
-
+    #region Attributes 
+    [Header("Jump")]
     [SerializeField]float jump;
     [SerializeField] bool isJumping;
+    [Space(5)]
 
     private Rigidbody2D rb;
-    // Start is called before the first frame update
+    #endregion
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -18,19 +20,24 @@ public class Jump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    #region Jump  
         if(Input.GetButtonDown("Jump") && !isJumping)
         {
             rb.AddForce(new Vector2(rb.velocity.x, jump));
             isJumping = true;
         }
     }
-
+        // check if touchground to reset jump
     void OnCollisionEnter2D(Collision2D other)
     {
         if(other.gameObject.CompareTag("Ground"))
         {
             isJumping = false;
+        }else
+        {
+            isJumping = true;
         }
     }
+
+    #endregion
 }
--- Assets/Scripts/simpMove.cs
+++ Assets/Scripts/simpMove.cs
@@ -7,17 +7,27 @@ public class simpMove : MonoBehaviour
     [SerializeField] float move;
     [SerializeField] float speed;
     [SerializeField] bool IsFacingRight;
+    [Space(5)]
+    [Header("Wall Slide")]
+    [SerializeField] bool isWallSliding;
+    [SerializeField] float wallSlideSpeed = 2f;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask wallLayer;
+  
     private Vector2 moveInput;
     private Rigidbody2D rb;
-    // Start is called before the first frame update
+   
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        #region Basic Movement
         move = Input.GetAxis("Horizontal");
         moveInput.x = move;
         rb.velocity = new Vector2(move * speed, rb.velocity.y);
@@ -26,9 +36,12 @@ public class simpMove : MonoBehaviour
 		{
         	CheckDirectionToFace(moveInput.x > 0);
         }
+        WallSlide();
 
+        #endregion
         
     }
+    
     private void Turn()
 	{
 		//stores scale and flips the player along the x axis, 
@@ -43,4 +56,21 @@ public class simpMove : MonoBehaviour
 		if (isMovingRight != IsFacingRight)
 			Turn();
 	}
+        // returns bool to check if touch wall
+    private bool IsWall()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position,0.2f, wallLayer);
+    }
+    // 
+    private void WallSlide()
+    {
+        if(IsWall() )
+        {
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlideSpeed, float.MaxValue));
+        }else
+        {
+            isWallSliding = false;
+        }
+    }
 }
