b056fe7cf64069b3ccf1dced2fab8bd3900f5c47
BellDylan373 <dylan.bellxd@gmail.com>
2024-10-23 22:29:42 -0500

    basic respawn
    respawns if fallen off level

---
+++ Assets/Scripts/Enviornment/Respawn.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+#region attributes
+    [SerializeField] public GameObject player;
+    [SerializeField] public GameObject respawnPoint;
+#endregion
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region Collisions
+      void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Get over Here!")
+         player.transform.position = respawnPoint.transform.position;
+        }
+       
+    }
+    #endregion
+}
